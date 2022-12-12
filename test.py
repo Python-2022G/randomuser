cddb9c5d042839775b24257785cf33a07cf2175c
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-12 18:40:56 +0500

    requirements.txt

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import requests
+from datetime import datetime
 
 
 while True:
     response = requests.get('https://randomuser.me/api/')
     if response.status_code == 200:
         user = response.json()['results'][0]
-        age = int(user['dob']['date'][:4])
-        if age in [1990, 1980, 1999, 1977, 1989, 1990]:
-            name = user['name']['first']
-            print(name, age)
+        dt = user['dob']['date']
+        dat = datetime.strptime(dt[:-5], '%Y-%m-%dT%H:%M:%S') #1958-02-09T05:22:53.242Z
+        if dat.year in [1990, 1980, 1989, 1991, 1977, 1968]:
+            print(dat)
+            break
